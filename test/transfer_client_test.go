25b952ca62889ca23279798af5f8cef55fae9949
FlyPig <yanglinbo@gmail.com>
2021-05-28 09:51:12 +0800

    first

---
+++ test/transfer_client_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/tx7do/huobi_futures_Golang/sdk/linearswap/restful"
+	"github.com/tx7do/huobi_futures_Golang/sdk/linearswap/restful/response/transfer"
+)
+
+var tfClient restful.TransferClient
+
+func init() {
+	tfClient = restful.TransferClient{}
+	tfClient.Init(config.AccessKey, config.SecretKey, "api.huobi.pro")
+}
+
+func TestTransferClient_TransferAsync(t *testing.T) {
+	data := make(chan transfer.TransferResponse)
+
+	go tfClient.TransferAsync(data, "spot", "linear-swap", 1, "BTC-USDT", "USDT")
+	x, ok := <-data
+	if !ok || x.Success != true {
+		t.Logf("%d:%s", x.Code, x.Message)
+		t.Fail()
+	} else {
+		t.Log(x)
+	}
+}
